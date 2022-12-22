776a06c8119350e94be291f91f07a8d2188bd8ce
sobolekovNA <n.sobolekov@gmail.com>
2022-12-22 07:52:07 +0500

    Добавлена возможность сохранять фотографии

---
+++ src/main/java/com/example/buysell/models/Image.java
@@ -0,0 +1,26 @@
+package com.example.buysell.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "images")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String originalFileName;
+    private Long size;
+    private String contentType;
+    private boolean isPreviewImage;
+    @Lob
+    private byte[] bytes;
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
+    private Product product;
+}
--- src/main/java/com/example/buysell/models/Product.java
+++ src/main/java/com/example/buysell/models/Product.java
@@ -5,6 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "products")
 @Data
@@ -13,11 +17,26 @@ import lombok.NoArgsConstructor;
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+    private Long id;
     private String title;
     @Column(columnDefinition = "text")
     private String description;
     private int price;
     private String city;
     private String author;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "product")
+    private List<Image> images = new ArrayList<>();
+    private Long previewImageId;
+    private LocalDateTime dateOfCreated;
+
+    @PrePersist
+    private void init() {
+        dateOfCreated = LocalDateTime.now();
+    }
+
+    public void addImageToProduct(Image image) {
+        image.setProduct(this);
+        images.add(image);
+    }
 }
